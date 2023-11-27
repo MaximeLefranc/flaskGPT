8a60b1d3bd0ce942c5d557aa7cb5a5f35a58733a
Maxime <maxilefranc@gmail.com>
2023-11-27 10:17:11 -0500

    get the response of openai API Done

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
+from typing import Generator, Sequence
+from pprint import pprint
 
 from flask import Flask, render_template
 import openai
+from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
 from dotenv import load_dotenv
 
 load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
+
+client = openai.OpenAI()
 
 app = Flask(__name__)
 
@@ -16,15 +21,28 @@ def home():
     return render_template('index.html')
 
 
-def build_conversation_dict(messages: list) -> list[dict]:
+def build_conversation_dict(messages: list):
     return [
         {
-            'role': 'user' if index % 2 == 0 else 'assistant',
+            'role': 'user' if index % 2 == 0 else 'system',
             'content': message
         }
         for index, message in enumerate(messages)
     ]
 
 
+def event_stream(conversation: list[dict]) -> Generator[str, None, None]:
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=conversation, stream=True)  # type: ignore
+
+    for line in response:
+        text = line.choices[0].delta.content
+        if text:
+            yield text
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=8080)
+    # conversation = build_conversation_dict(messages=['Bonjour, comment ça va ?', 'Ça va bien et toi ?'])
+    # event_stream(conversation=conversation)
+    # for line in event_stream(conversation=conversation):
+    #     print(line)
