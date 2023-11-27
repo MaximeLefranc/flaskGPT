07ed26584e9e05fbac97ebc04be32d15906fb4ac
Maxime <maxilefranc@gmail.com>
2023-11-27 09:25:20 -0500

    definition of build_conversation_dict function who will build an list[dict] for prepare the API call

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+import os
+
 from flask import Flask, render_template
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
 
@@ -6,6 +14,16 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+def build_conversation_dict(messages: list) -> list[dict]:
+    return [
+        {
+            'role': 'user' if index % 2 == 0 else 'assistant',
+            'content': message
+        }
+        for index, message in enumerate(messages)
+    ]
 
 
 if __name__ == '__main__':
